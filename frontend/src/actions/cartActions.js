380cede0f27581acac44a9b49d410ddbe907f132
IsuruAET <isuru.g@aeturnum.com>
2021-03-17 13:11:16 +0530

    DEV-026-Remove-Items-From-Cart

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants'
+import { PRODUCT_DETAILS_FAIL } from '../constants/productConstants'
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
   try {
@@ -18,5 +19,19 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
     })
 
     localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
-  } catch (error) {}
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
+      payload: error.response && error.response.data.message ? error.response.data.message : error.message
+    })
+  }
+}
+
+export const removeFromCart = (id) => async (dispatch, getState) => {
+  dispatch({
+    type: CART_REMOVE_ITEM,
+    payload: id
+  })
+
+  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
 }
