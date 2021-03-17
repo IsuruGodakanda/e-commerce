7857ce2bc43454e6c47601833cab6dd7480eef1a
IsuruAET <isuru.g@aeturnum.com>
2021-03-18 00:51:34 +0530

    DEV-033-User-Login-Screen-And-Functionality

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGIN_FAIL, USER_LOGOUT } from '../constants/userConstants'
 
-export const login = ({ email, password }) => async (dispatch) => {
+export const login = (email, password) => async (dispatch) => {
   try {
     dispatch({ type: USER_LOGIN_REQUEST })
 
