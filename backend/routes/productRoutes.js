43055e6125142ee5e2adc767d05076f10091b977
IsuruAET <isuru.g@aeturnum.com>
2021-03-17 13:25:26 +0530

    DEV-027-Clean-Up-By-Using-Controllers

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,34 +1,9 @@
 import express from 'express'
-import asyncHandler from 'express-async-handler'
 const router = express.Router()
-import Product from '../models/productModel.js'
+import { getProducts, getProductById } from '../controllers/productController.js'
 
-// @desc    Fetch all products
-// @route   GET /api/products
-// @access  Public
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({})
-    res.send(products)
-  })
-)
+router.route('/').get(getProducts)
 
-// @desc    Fetch single product
-// @route   GET /api/products/:id
-// @access  Public
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id)
-
-    if (product) {
-      res.send(product)
-    } else {
-      console.log('object')
-      throw new Error('Product not found')
-    }
-  })
-)
+router.route('/:id').get(getProductById)
 
 export default router
