062f9dc25e423346a80eea97bc7c226e160e4f72
IsuruAET <isuru.g@aeturnum.com>
2021-03-19 18:42:37 +0530

    DEV-041-Payment-Screen-And-Save-Payment-Method

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,5 +1,10 @@
 import axios from 'axios'
-import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_SHIPPING_ADDRESS } from '../constants/cartConstants'
+import {
+  CART_ADD_ITEM,
+  CART_REMOVE_ITEM,
+  CART_SAVE_SHIPPING_ADDRESS,
+  CART_SAVE_PAYMENT_METHOD
+} from '../constants/cartConstants'
 import { PRODUCT_DETAILS_FAIL } from '../constants/productConstants'
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
@@ -43,4 +48,13 @@ export const saveShippingAddress = (data) => async (dispatch) => {
   })
 
   localStorage.setItem('shippingAddress', JSON.stringify(data))
+}
+
+export const savePaymentMethod = (data) => async (dispatch) => {
+  dispatch({
+    type: CART_SAVE_PAYMENT_METHOD,
+    payload: data
+  })
+
+  localStorage.setItem('paymentMethod', JSON.stringify(data))
 }
