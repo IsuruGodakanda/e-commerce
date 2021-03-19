fd3c712e947f73838332f091e993dfb67fa0de7a
IsuruAET <isuru.g@aeturnum.com>
2021-03-19 15:38:47 +0530

    DEV-039-Shipping-Screen-And-Save-Address

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants'
+import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_SHIPPING_ADDRESS } from '../constants/cartConstants'
 import { PRODUCT_DETAILS_FAIL } from '../constants/productConstants'
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
@@ -34,4 +34,13 @@ export const removeFromCart = (id) => async (dispatch, getState) => {
   })
 
   localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
+}
+
+export const saveShippingAddress = (data) => async (dispatch) => {
+  dispatch({
+    type: CART_SAVE_SHIPPING_ADDRESS,
+    payload: data
+  })
+
+  localStorage.setItem('shippingAddress', JSON.stringify(data))
 }
