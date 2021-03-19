e5cd003ee4fa642fa93eec6577765edab9b2da91
IsuruAET <isuru.g@aeturnum.com>
2021-03-20 02:34:07 +0530

    DEV-048-BE-Update-To-Paid-Endpoint

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express'
 const router = express.Router()
-import { addOrderItems, getOrderById } from '../controllers/orderController.js'
+import { addOrderItems, getOrderById, updateOrderToPaid } from '../controllers/orderController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
 router.post('/', protect, addOrderItems)
 
 router.get('/:id', protect, getOrderById)
+
+router.put('/:id/pay', protect, updateOrderToPaid)
 
 export default router
