64c6e773a7efd3ae09add2b7be06d16aa888742f
IsuruAET <isuru.g@aeturnum.com>
2021-03-20 04:33:37 +0530

    DEV-050-Adding-PayPal-Payments

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -43,6 +43,8 @@ export const createOrder = (order) => async (dispatch, getState) => {
 
 export const getOrderDetails = (id) => async (dispatch, getState) => {
   try {
+    dispatch({ type: ORDER_PAY_RESET })
+
     dispatch({ type: ORDER_DETAILS_REQUEST })
 
     const {
