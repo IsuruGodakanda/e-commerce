1db290194718823b562e1759be9408f5e93ca4af
IsuruAET <isuru.g@aeturnum.com>
2021-03-20 14:32:49 +0530

    DEV-058-User-Edit-Screen-And-Get-User-Details

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -55,7 +55,6 @@ const OrderScreen = ({ match }) => {
   }, [dispatch, order, orderId, successPay])
 
   const successPaymentHandler = (paymentResult) => {
-    console.log(paymentResult)
     dispatch(payOrder(orderId, paymentResult))
   }
 
