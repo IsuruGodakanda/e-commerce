fda2bad58660e80e11057699249ddaaa0fdfd654
IsuruAET <isuru.g@aeturnum.com>
2021-03-20 12:56:15 +0530

    DEV-056-Admin-User-DeleteDEV-056-Admin-User-Delete

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,13 @@
 import express from 'express'
 const router = express.Router()
-import { registerUser, authUser, getUserProfile, updateUserProfile, getUsers } from '../controllers/userController.js'
+import {
+  registerUser,
+  authUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  deleteUser
+} from '../controllers/userController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
 router.post('/', registerUser)
@@ -12,5 +19,7 @@ router.post('/login', authUser)
 router.get('/profile', protect, getUserProfile)
 
 router.put('/profile', protect, updateUserProfile)
+
+router.delete('/:id', protect, admin, deleteUser)
 
 export default router
