6e1047679b54064c0dc1a751dc100bb16a4564db
IsuruAET <isuru.g@aeturnum.com>
2021-03-21 13:32:49 +0530

    DEV-065-Admin-Update-Product

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -5,13 +5,18 @@ import {
   PRODUCT_DETAILS_REQUEST,
   PRODUCT_DETAILS_SUCCESS,
   PRODUCT_DETAILS_FAIL,
+  PRODUCT_DETAILS_RESET,
   PRODUCT_DELETE_REQUEST,
   PRODUCT_DELETE_SUCCESS,
   PRODUCT_DELETE_FAIL,
   PRODUCT_CREATE_REQUEST,
   PRODUCT_CREATE_SUCCESS,
   PRODUCT_CREATE_FAIL,
-  PRODUCT_CREATE_RESET
+  PRODUCT_CREATE_RESET,
+  PRODUCT_UPDATE_REQUEST,
+  PRODUCT_UPDATE_SUCCESS,
+  PRODUCT_UPDATE_FAIL,
+  PRODUCT_UPDATE_RESET
 } from '../constants/productConstants'
 
 export const productListReducer = (state = { products: [] }, action) => {
@@ -35,6 +40,8 @@ export const productDetailsReducer = (state = { product: { reviews: [] } }, acti
       return { loading: false, product: action.payload }
     case PRODUCT_DETAILS_FAIL:
       return { loading: false, error: action.payload }
+    case PRODUCT_DETAILS_RESET:
+      return { product: { reviews: [] } }
     default:
       return state
   }
@@ -63,6 +70,21 @@ export const productCreateReducer = (state = {}, action) => {
       return { loading: false, error: action.payload }
     case PRODUCT_CREATE_RESET:
       return {}
+    default:
+      return state
+  }
+}
+
+export const productUpdateReducer = (state = { product: {} }, action) => {
+  switch (action.type) {
+    case PRODUCT_UPDATE_REQUEST:
+      return { loading: true }
+    case PRODUCT_UPDATE_SUCCESS:
+      return { loading: false, success: true, product: action.payload }
+    case PRODUCT_UPDATE_FAIL:
+      return { loading: false, error: action.payload }
+    case PRODUCT_UPDATE_RESET:
+      return { product: {} }
     default:
       return state
   }
