94cc62a41f88ae8bb035ebce49ad2138667e2978
IsuruAET <isuru.g@aeturnum.com>
2021-03-22 03:28:39 +0530

    DEV-073-Top-Product-Carousel

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,13 +6,16 @@ import {
   deleteProduct,
   createProduct,
   updateProduct,
-  createProductReview
+  createProductReview,
+  getTopProducts
 } from '../controllers/productController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
 router.get('/', getProducts)
 
 router.post('/', protect, admin, createProduct)
+
+router.get('/top', getTopProducts)
 
 router.get('/:id', getProductById)
 
